e62bf3b462443393443b8d1ec60933097f7d9c2d
Jeremiah Norris <jjno91@gmail.com>
2019-02-03 19:28:42 -0600

    correct volume size variable name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_ebs_volume" "this" {
   availability_zone = "${var.availability_zone}"
-  size              = "${var.volume_size}"
+  size              = "${var.size}"
   snapshot_id       = "${var.snapshot_id}"
   encrypted         = "${var.encrypted}"
   type              = "gp2"
